2a8e883b4b9e2972566419b51a1adc94a2cbe0ac
moreescudero <escuderomore1@gmail.com>
2026-05-31 18:52:16 -0300

    fix: add CORS policy for Vercel frontend

--- CampusClash.API/Program.cs
+++ CampusClash.API/Program.cs
@@ -83,7 +83,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
