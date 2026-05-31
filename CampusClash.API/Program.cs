5e50ee964e4fe49380f264b46d23cbee1d872a6b
moreescudero <escuderomore1@gmail.com>
2026-05-31 19:01:56 -0300

    fix: auto-run migrations on startup

--- CampusClash.API/Program.cs
+++ CampusClash.API/Program.cs
@@ -75,6 +75,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseCors("AllowFrontend");
 
 if (app.Environment.IsDevelopment())
